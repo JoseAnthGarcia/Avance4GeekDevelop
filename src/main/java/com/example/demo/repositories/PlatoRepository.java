57c1f11e84da9500f269c8a0ae197e7980f87f73
Jose Garcia - Acer <garcia.josea@pucp.edu.pe>
2021-05-01 14:08:54 -0500

    ---

--- src/main/java/com/example/demo/repositories/PlatoRepository.java
+++ src/main/java/com/example/demo/repositories/PlatoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRestRepository extends JpaRepository<Plato, Integer> {
+public interface PlatoRepository extends JpaRepository<Plato, Integer> {
 
 }

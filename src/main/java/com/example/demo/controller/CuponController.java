4567c8e91f27948762dd013fc396fa9846389ce6
JohamRV <a20181459@pucp.edu.pe>
2021-05-01 14:39:25 -0500

    intento de validaciones

--- src/main/java/com/example/demo/controller/CuponController.java
+++ src/main/java/com/example/demo/controller/CuponController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,16 +35,20 @@ public class CuponController {
     }
 
     @PostMapping("/guardar")
-    public String guardarCupon(Cupon cupon, RedirectAttributes attributes){
-        Cupon cupon1=cupon;
-        System.out.println(cupon1.getIdcupon());
-        if (cupon.getIdcupon()==0){
-            attributes.addFlashAttribute("creado","Cupon creado exitosamente!");
-        }else {
-            attributes.addFlashAttribute("editado","Cupon editado exitosamente!");
+    public String guardarCupon(@ModelAttribute("cupon") @Valid Cupon cupon, BindingResult bindingResult, RedirectAttributes attributes, Model model){
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("cupon",cupon);
+            return "redirect:/cupon/editar";
+        }else{
+            if (cupon.getIdcupon()==0){
+                attributes.addFlashAttribute("creado","Cupon creado exitosamente!");
+            }else {
+                attributes.addFlashAttribute("editado","Cupon editado exitosamente!");
+            }
+            cuponRepository.save(cupon);
+            return "redirect:/cupon/lista";
         }
-        cuponRepository.save(cupon);
-        return "redirect:/cupon/lista";
     }
 
     @GetMapping("/editar")

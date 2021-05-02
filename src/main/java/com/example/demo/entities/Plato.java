30640845b17f8ee87e773fa1f9577f50150e3405
Jose Garcia - Acer <garcia.josea@pucp.edu.pe>
2021-05-01 21:30:43 -0500

    validaciones editarPlato.html y nuevoPlato.html

--- src/main/java/com/example/demo/entities/Plato.java
+++ src/main/java/com/example/demo/entities/Plato.java
@@ -1,5 +1,7 @@
 package com.example.demo.entities;
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name="plato")
@@ -9,11 +11,18 @@ public class Plato {
     @Column(name="idplato")
     private int idplato;
     @Column(name="nombre", nullable = false)
+    @NotBlank(message = "Este campo es obligatorio")
+    @Size(max=40, message = "Maximo 40 caracteres")
     private String nombre;
-    @Column(name="precio", nullable = false)
-    private double precio;
     @Column(name="descripcion", nullable = false)
+    @NotBlank(message = "Este campo es obligatorio")
+    @Size(max=100, message = "Maximo 100 caracteres")
     private String descripcion;
+    @Column(name="precio", nullable = false)
+    @Positive(message = "Ingrese una cantidad positiva")
+    @Digits(integer=10, fraction = 2, message = "Ingrese un precio valido")
+    @NotNull(message = "Este campo es obligatorio")
+    private BigDecimal precio;
     @Column(name="idcategoriaplato", nullable = false)
     private int idcategoriaplato;
     @Column(name="idrestaurante", nullable = false)
@@ -38,11 +47,11 @@ public class Plato {
         this.nombre = nombre;
     }
 
-    public double getPrecio() {
+    public BigDecimal getPrecio() {
         return precio;
     }
 
-    public void setPrecio(double precio) {
+    public void setPrecio(BigDecimal precio) {
         this.precio = precio;
     }
 

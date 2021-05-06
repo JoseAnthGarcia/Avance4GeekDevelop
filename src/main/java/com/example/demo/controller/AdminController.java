2190affb2d851ecdb1351268810082a9c3c4bb6c
Jose Garcia - Acer <garcia.josea@pucp.edu.pe>
2021-05-05 21:08:50 -0500

    aceptar/rechazar repartidor sin validadciones

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entities.Usuario;
 import com.example.demo.repositories.MovilidadRepository;
+import com.example.demo.repositories.TipoMovilidadRepository;
 import com.example.demo.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -37,6 +39,7 @@ public class AdminController {
         if(tipo == null){
             tipo = "adminRest";
         }
+        model.addAttribute("listaTipoMovilidad", tipoMovilidadRepository.findAll());
         switch (tipo){
             case "restaurante":
                 return "";
@@ -52,6 +55,35 @@ public class AdminController {
                 return "";
                 //mandar a la vista principal
         }
+    }
+
+    @PostMapping("/buscadorSoli")
+    public String aceptarSolitud(@RequestParam(value = "nombreUsuario", required = false) String nombreUsuario1,
+                                 @RequestParam(value = "tipoMovilidad", required = false) Integer tipoMovilidad1,
+                                 @RequestParam(value = "fechaRegistro", required = false) Integer fechaRegistro1,
+                                 Model model){
+        System.out.println(nombreUsuario1+", "+tipoMovilidad1+", "+fechaRegistro1);
+        model.addAttribute("nombreUsuario1", nombreUsuario1);
+        model.addAttribute("tipoMovilidad1", tipoMovilidad1);
+        model.addAttribute("fechaRegistro1", fechaRegistro1);
+
+        if(fechaRegistro1==null){
+            fechaRegistro1 = usuarioRepository.buscarFechaMinimaRepartidor();
+        }
+
+
+        model.addAttribute("listaTipoMovilidad", tipoMovilidadRepository.findAll());
+        if(tipoMovilidad1==null){
+            model.addAttribute("listaRepartidorSolicitudes", usuarioRepository.buscarRepartidoresSinMovilidad(nombreUsuario1, fechaRegistro1*-1));
+        }else{
+            model.addAttribute("listaRepartidorSolicitudes", usuarioRepository.buscarRepartidoresConMovilidad(nombreUsuario1, fechaRegistro1*-1, tipoMovilidad1));
+        }
+
+        System.out.println("------------");
+        System.out.println(nombreUsuario1+", "+tipoMovilidad1+", "+fechaRegistro1);
+        System.out.println("------------");
+
+        return "/AdminGen/solicitudRepartidor";
     }
 
     @GetMapping("/aceptarSolicitud")
@@ -71,7 +103,27 @@ public class AdminController {
                 usuario.setFechaadmitido(hourdateFormat.format(date));
                 //
                 usuarioRepository.save(usuario);
-                return "/AdminGen/solicitudRepartidor";
+                return "redirect:/admin/solicitudes?tipo=repartidor";
+            }else{
+                return ""; //Retornar pagina principal
+            }
+        }
+
+    }
+
+    @GetMapping("/rechazarSolicitud")
+    public String rechazarSolicitud(@RequestParam(value = "id", required = false) Integer id){
+
+        if(id == null){
+            return ""; //Retornar pagina principal
+        }else {
+            Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+
+            if(usuarioOpt.isPresent()){
+                Usuario usuario = usuarioOpt.get();
+                usuario.setEstado("REHAZADO");
+                usuarioRepository.save(usuario);
+                return "redirect:/admin/solicitudes?tipo=repartidor";
             }else{
                 return ""; //Retornar pagina principal
             }
--- src/main/java/com/example/demo/repositories/UsuarioRepository.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepository.java
@@ -15,4 +15,26 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     @Query(value = "select * from usuario where estado = 'bloqueado' or estado = 'activo' ", nativeQuery = true)
     List<Usuario> listaUsuariosAceptados();
 
+    @Query(value = "select datediff(now(),min(fechaRegistro)) from usuario", nativeQuery = true)
+    int buscarFechaMinimaRepartidor();
+
+    @Query(value = "select min(movilidad) from usuario", nativeQuery = true)
+    int buscarIdMovilidadMinimaRepartidor();
+
+    @Query(value = "select u.* from usuario u\n" +
+            "left join movilidad m on u.movilidad = m.idmovilidad\n" +
+            "where u.rol='repartidor' and\n" +
+            "u.estado = 'PENDIENTE' and \n" +
+            "lower(u.nombres) like %?1% and\n" +
+            "u.fechaRegistro>= DATE_ADD(now(), INTERVAL ?2 DAY)", nativeQuery = true)
+    List<Usuario> buscarRepartidoresSinMovilidad(String nombres, int fechaRegistro);
+
+    @Query(value = "select u.* from usuario u\n" +
+            "left join movilidad m on u.movilidad = m.idmovilidad\n" +
+            "where u.rol='repartidor' and\n" +
+            "u.estado = 'PENDIENTE' and \n" +
+            "lower(u.nombres) like %?1% and\n" +
+            "u.fechaRegistro>= DATE_ADD(now(), INTERVAL ?2 DAY) and m.idtipomovilidad = ?3", nativeQuery = true)
+    List<Usuario> buscarRepartidoresConMovilidad(String nombres, int fechaRegistro, int idMovilidad);
+
 }

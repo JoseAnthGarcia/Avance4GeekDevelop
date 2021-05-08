e466b8695004b2f7fd2f52d38c9f2000b8d6dfe1
Melanie <melanie.espinoza@pucp.edu.pe>
2021-05-08 18:58:16 -0500

    NOOOOOO3

--- src/main/java/com/example/demo/controller/ClienteController.java
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.entities.Distrito;
 import com.example.demo.entities.Usuario;
+import com.example.demo.entities.Usuario_has_distrito;
+import com.example.demo.entities.Usuario_has_distritoKey;
+import com.example.demo.repositories.DistritosRepository;
 import com.example.demo.repositories.RolRepository;
 import com.example.demo.repositories.UsuarioRepository;
+import com.example.demo.repositories.Usuario_has_distritoRepository;
+import org.apache.tomcat.util.modeler.BaseAttributeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/cliente")
@@ -22,31 +29,72 @@ public class ClienteController {
     @Autowired
     RolRepository rolRepository;
 
+    @Autowired
+    DistritosRepository distritosRepository;
+
+    @Autowired
+    Usuario_has_distritoRepository usuario_has_distritoRepository;
+
+
     @GetMapping("/login")
     public String loginCliente() {
         return "Cliente/login";
     }
 
     @GetMapping("/nuevo")
-    public String nuevoCliente(@ModelAttribute("cliente") Usuario cliente) {
+    public String nuevoCliente(@ModelAttribute("cliente") Usuario cliente, Model model) {
+        String direccion;
+        model.addAttribute("Usuario_has_distrito",new Usuario_has_distrito()) ;
+        //distritos
+        model.addAttribute("distrito", new Distrito());
+        //distritos -->
+        model.addAttribute("listaDistritos", distritosRepository.findAll());
         return "Cliente/registro";
+
     }
 
     @PostMapping("/guardar")
-    public String guardarCliente(@ModelAttribute("cliente") Usuario cliente,
-                                    Model model, RedirectAttributes attr) {
-        //int tamañoListaCredenciales = 0;
+    public String guardarCliente(@ModelAttribute("cliente") Usuario cliente,Usuario_has_distrito usuario_has_distrito,
+                                 Distrito distrito,Model model, RedirectAttributes attr) {
+
+            //setear direccion
+
 
             cliente.setEstado(1);
             cliente.setRol(rolRepository.findById(1).get());
             String fechanacimiento = LocalDate.now().toString();
             cliente.setFecharegistro(fechanacimiento);
-
             clienteRepository.save(cliente);
             attr.addFlashAttribute("msg", "Cliente creado exitosamente");
+
+
+            clienteRepository.save(cliente);
+
+
+            //SETEO EXTERNO
+
+
+            //Guaradando el seleccionado
+        //todo ya esta arriba
+            // Distrito distrito=cliente.getDistritos().get(0);
+
+            Usuario_has_distritoKey usuario_has_distritoKey = new Usuario_has_distritoKey();
+            //SETEANDO ID'S
+            usuario_has_distritoKey.setIddistrito(distrito.getIddistrito());
+            usuario_has_distritoKey.setIdusuario(cliente.getIdusuario());
+
+            //todo ya c recibio
+            //Usuario_has_distrito usuario_has_distrito = new Usuario_has_distrito();
+            usuario_has_distrito.setId(usuario_has_distritoKey);
+
+            usuario_has_distrito.setDistrito(distrito);
+            usuario_has_distrito.setUsuario(cliente);
+            //usuario_has_distrito.setDireccion();
+            usuario_has_distritoRepository.save(usuario_has_distrito);
+
+
+
             return "redirect:/cliente/login";
-
-
 
     }
 }

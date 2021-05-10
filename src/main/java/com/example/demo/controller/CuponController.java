521350dcfe7d63b15a6abcdf90165e9760bc16af
JohamRV <a20181459@pucp.edu.pe>
2021-05-10 02:30:48 -0500

    cupon

--- src/main/java/com/example/demo/controller/CuponController.java
+++ src/main/java/com/example/demo/controller/CuponController.java
@@ -32,7 +32,7 @@ public class CuponController {
     }
 
 
-/*
+
     @GetMapping("/nuevo")
     public String nuevoCupon(@ModelAttribute("cupon") Cupon cupon) {
         return "AdminRestaurante/nuevoCupon";
@@ -42,35 +42,28 @@ public class CuponController {
     public String guardarCupon(@ModelAttribute("cupon") @Valid Cupon cupon, BindingResult bindingResult,
                                RedirectAttributes attributes,
                                Model model) {
-        Cupon cVal = cuponRepository.buscarPorNombre(cupon.getNombre());
+        //Cupon cVal = cuponRepository.buscarPorNombre(cupon.getNombre());
 
-        if(cVal == null){
-            if(bindingResult.hasErrors()){
-                return "AdminRestaurante/nuevoCupon";
-            }
+        if(bindingResult.hasErrors()){
+            return "AdminRestaurante/nuevoCupon";
+        }
 
-            cupon.setIdrestaurante(2);
+        cupon.setIdrestaurante(2);
 
-            if (cupon.getIdcupon() == 0) {
-                cupon.setFechainicio(LocalDate.now());
-                cupon.setEstado(1);
-                attributes.addFlashAttribute("creado", "Cupon creado exitosamente!");
-            } else {
-                attributes.addFlashAttribute("editado", "Cupon editado exitosamente!");
-            }
+        if (cupon.getIdcupon() == 0) {
+            cupon.setFechainicio(LocalDate.now());
+            cupon.setEstado(1);
+            attributes.addFlashAttribute("creado", "Cupon creado exitosamente!");
+        } else {
+            attributes.addFlashAttribute("editado", "Cupon editado exitosamente!");
+        }
 
             /*if(cupon.getFechainicio().isEqual(cupon .getFechafin())){
                 cupon.setDisponible(false);
             }*/
 
-            cuponRepository.save(cupon);
-            return "redirect:/cupon/lista";
-        }else{
-           model.addAttribute("val","Este nombre ya está registrado");
-            return "AdminRestaurante/nuevoCupon";
-        }
-
-
+        cuponRepository.save(cupon);
+        return "redirect:/cupon/lista";
     }
 
     @GetMapping("/editar")
@@ -95,10 +88,8 @@ public class CuponController {
             cupon.setEstado(0);
             cuponRepository.save(cupon);
             attr.addFlashAttribute("bloqueo","Cupón bloqueado exitosamente");
-            return "AdminRestaurante/nuevoCupon";
-        } else {
-            return "redirect:/cupon/lista";
         }
+        return "redirect:/cupon/lista";
     }
     @GetMapping("/publicar")
     public String publicarCupon(@RequestParam("id") int id, RedirectAttributes attr){

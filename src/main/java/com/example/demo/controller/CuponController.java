7a371eeb1107dcd9d3ca4a54c89b59fe215067e9
Fabrizio <a20160348@pucp.edu.pe>
2021-05-10 01:51:45 -0500

    vista editar y nuevo, extra y plato

--- src/main/java/com/example/demo/controller/CuponController.java
+++ src/main/java/com/example/demo/controller/CuponController.java
@@ -32,7 +32,7 @@ public class CuponController {
     }
 
 
-
+/*
     @GetMapping("/nuevo")
     public String nuevoCupon(@ModelAttribute("cupon") Cupon cupon) {
         return "AdminRestaurante/nuevoCupon";

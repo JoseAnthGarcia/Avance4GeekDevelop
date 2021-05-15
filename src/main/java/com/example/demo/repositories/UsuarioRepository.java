3cf50717596686ad8c7cff90a496374364bae5d8
Jose Garcia - Acer <garcia.josea@pucp.edu.pe>
2021-05-14 20:41:30 -0500

    -

--- src/main/java/com/example/demo/repositories/UsuarioRepository.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepository.java
@@ -19,31 +19,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Page<Usuario> findByEstadoAndRolOrderByFecharegistroAsc(int estado, Rol rol, Pageable pageable);
 
-    @Query(value = "select datediff(now(),min(fechaRegistro)) from usuario", nativeQuery = true)
-    int buscarFechaMinimaRepartidor();
-    /*
-
-    @Query(value = "select u.* from usuario u\n" +
-            "left join movilidad m on u.idmovilidad = m.idmovilidad\n" +
-            "where u.idrol=4 and\n" +
-            "u.estado = 2 and\n" +
-            "(lower(u.nombres) like %?1% or lower(u.apellidos) like %?2%) and\n" +
-            "u.fechaRegistro>= DATE_ADD(now(), INTERVAL ?3 DAY)", nativeQuery = true)
-    Page<Usuario> buscarRepartidoresSinMovilidad(String nombres, String apellidos, int fechaRegistro, Pageable pageable);
-
-    @Query(value = "select u.* from usuario u\n" +
-            "left join movilidad m on u.idmovilidad = m.idmovilidad\n" +
-            "where u.idrol=4 and\n" +
-            "u.estado = 2 and\n" +
-            "(lower(u.nombres) like %?1% or lower(u.apellidos) like %?2%) and\n" +
-            "u.fechaRegistro>= DATE_ADD(now(), INTERVAL ?3 DAY) and m.idtipomovilidad = ?4", nativeQuery = true)
-    Page<Usuario> buscarRepartidoresConMovilidad(String nombres,String apellidos, int fechaRegistro, int idMovilidad, Pageable pageable);
-
-*/
-
-    @Query(value = "select min(movilidad) from usuario", nativeQuery = true)
-    int buscarIdMovilidadMinimaRepartidor(Pageable pageable);
-
 
     @Query(value = "select * from usuario where dni = ?1", nativeQuery = true)
     Usuario findByDni (String dni);
@@ -53,6 +28,28 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     @Query(value =" select * from usuario u, rol r where u.idrol = r.idrol and (u.estado = 0 or u.estado = 1) and r.tipo != 'administradorG' " , nativeQuery = true)
     List<Usuario> listaUsuarios();
+
+    @Query(value = "select datediff(now(),min(fechaRegistro)) from usuario", nativeQuery = true)
+    int buscarFechaMinimaRepartidor();
+
+    @Query(value = "select min(movilidad) from usuario", nativeQuery = true)
+    int buscarIdMovilidadMinimaRepartidor();
+
+    @Query(value = "select u.* from usuario u\n" +
+            "left join movilidad m on u.idmovilidad = m.idmovilidad\n" +
+            "where u.idrol=4 and\n" +
+            "u.estado = 2 and\n" +
+            "(lower(u.nombres) like %?1% or lower(u.apellidos) like %?2%) and\n" +
+            "u.fechaRegistro>= DATE_ADD(now(), INTERVAL ?3 DAY)", nativeQuery = true)
+    List<Usuario> buscarRepartidoresSinMovilidad(String nombres,String apellidos, int fechaRegistro);
+
+    @Query(value = "select u.* from usuario u\n" +
+            "left join movilidad m on u.idmovilidad = m.idmovilidad\n" +
+            "where u.idrol=4 and\n" +
+            "u.estado = 2 and\n" +
+            "(lower(u.nombres) like %?1% or lower(u.apellidos) like %?2%) and\n" +
+            "u.fechaRegistro>= DATE_ADD(now(), INTERVAL ?3 DAY) and m.idtipomovilidad = ?4", nativeQuery = true)
+    List<Usuario> buscarRepartidoresConMovilidad(String nombres,String apellidos, int fechaRegistro, int idMovilidad);
 
     Usuario findByCorreo(String correo);
 

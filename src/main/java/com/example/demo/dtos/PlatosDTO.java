dce7c9a4fe56e567dc6e181e7b798686783cdada
JohamRV <a20181459@pucp.edu.pe>
2021-05-26 03:05:30 -0500

    lista productos - 50%

--- src/main/java/com/example/demo/dtos/PlatosDTO.java
+++ src/main/java/com/example/demo/dtos/PlatosDTO.java
@@ -1,4 +1,14 @@
 package com.example.demo.dtos;
 
+import java.math.BigDecimal;
+
 public interface PlatosDTO {
+
+    int getIdPlato();
+    int getNombre();
+    BigDecimal getPrecio();
+    byte[] getFoto();
+    String getFotocontenttype();
+    String getFotonombre();
+
 }
